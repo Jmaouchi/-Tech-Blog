6775aefc0c7ede9bac5a87a5b3077fea5cd98061
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-12 20:45:42 -0500

    added logout functionality to the page and cleaned handlebars

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment} = require('../models');
+const withAuth = require('../utils/auth');
+
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+    ],
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 //get all the routes from the api folder 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
 
 router.use((req, res) => {
   res.status(404).end();
